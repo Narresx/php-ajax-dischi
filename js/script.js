64bab053b3331edaedb045c3838a8de2404029f9
Narresx <92748159+Narresx@users.noreply.github.com>
2022-03-01 18:24:45 +0100

    add  disks by Html, CSS, VueJS, axios

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: '#root',
     data: {
+        disks: [],
+    },
 
+    mounted() {
+        axios.get('http://localhost/php-ajax-dischi/data/database.php').then(res => {
+            this.disks = res.data;
+        })
     },
 })
